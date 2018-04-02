28ccc627ae3c37dbd040b6e9a4696587d416c4ac
Jason Abbott <dev@trailimage.com>
2018-04-01 20:05:35 -0600

    update video info

--- lib/video-info.ts
+++ lib/video-info.ts
@@ -1,6 +1,14 @@
 import { Flickr } from '@toba/flickr';
 import { VideoInfo } from '@trailimage/models';
-import re from '../regex';
+//import re from '../regex';
+
+/**
+ * Extract video link from specially formatted remark in set description.
+ * Example:
+ *
+ *    Video (960x720): <a href="http://youtu.be/obCgu3yJ4uw" rel="nofollow">youtu.be/obCgu3yJ4uw</a>
+ */
+const re = /Video(\s*\((\d+)[x×](\d+)\))?:\s*<a[^>]+>[^\/]+\/([\w\-_]+)<\/a>/gi;
 
 /**
  * Get video ID and dimensions
@@ -8,8 +16,8 @@ import re from '../regex';
 export function make(setInfo: Flickr.SetInfo): VideoInfo {
    const d = setInfo.description._content;
 
-   if (re.video.test(d)) {
-      const match = re.video.exec(d);
+   if (re.test(d)) {
+      const match = re.exec(d);
       // remove video link from description
       setInfo.description._content = d.replace(match[0], '');
       return new VideoInfo(match[4], parseInt(match[2]), parseInt(match[3]));
