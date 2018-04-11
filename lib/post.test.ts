8d9e60dc34a835ba2225afaa0f565f0842052081
Jason Abbott <dev@trailimage.com>
2018-04-10 18:52:48 -0600

    more post tests

--- lib/post.test.ts
+++ lib/post.test.ts
@@ -2,7 +2,7 @@ import '@toba/test';
 import './client.test';
 import { Flickr } from '@toba/flickr';
 import { flickr } from './client';
-import { loadPost, timeStampToDate } from './post';
+import { loadPost, timeStampToDate, loadInfo, loadPhotos } from './post';
 
 let res: Flickr.Collection[];
 
@@ -21,4 +21,26 @@ test('Loads post from Flickr data', () => {
    expect(post).toBeDefined();
    expect(post.id).toBe('72157666725213214');
    expect(post.title).toBe('Stanley Lake Snow Hike');
+   expect(post.infoLoaded).toBe(false);
+   expect(post.photosLoaded).toBe(false);
+});
+
+test('Loads post info from Flickr data', async () => {
+   const post = loadPost(res[0].collection[0].set[0]);
+
+   await loadInfo(post);
+
+   expect(post.infoLoaded).toBe(true);
+   expect(post.photosLoaded).toBe(false);
+});
+
+test('Loads photo info from Flickr data', async () => {
+   const post = loadPost(res[0].collection[0].set[0]);
+
+   await loadPhotos(post);
+
+   expect(post.infoLoaded).toBe(false);
+   expect(post.photosLoaded).toBe(true);
+   expect(post.photos.length).toBe(13);
+   expect(post.photos[0].title).toBe('Slow roasted');
 });
