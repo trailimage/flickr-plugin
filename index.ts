d4c480c2cd127fb2b1022dd6ebe0266036451732
Jason Abbott <dev@trailimage.com>
2018-04-21 10:35:46 -0600

    export flickr namespace

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export { provider } from './lib/provider';
 export { configure } from './lib/client';
-export { FlickrConfig } from '@toba/flickr';
+export { FlickrConfig, Flickr } from '@toba/flickr';
