7f1a26ed32d98c7e25b1dede5f0b9f2211946ff1
Jason Abbott <jasonabbott@Jason-Abbot-MBP15tb.local>
2018-06-15 12:42:08 -0600

    implement class instead of interface

--- lib/provider.ts
+++ lib/provider.ts
@@ -1,14 +1,26 @@
 import { PostProvider } from '@trailimage/models';
 import { loadPhotoBlog } from './photo-blog';
+import { ProviderConfig } from './index';
 import { photosWithTags } from './photo';
 import { loadInfo, loadPhotos, postIdWithPhotoId } from './post';
 import { loadEXIF } from './exif';
 
-export const provider: PostProvider = {
-   photoBlog: loadPhotoBlog,
-   exif: loadEXIF,
-   postIdWithPhotoId,
-   photosWithTags,
-   postInfo: loadInfo,
-   postPhotos: loadPhotos
-};
+class FlickrProvider extends PostProvider<ProviderConfig> {
+   photoBlog = loadPhotoBlog;
+   exif = loadEXIF;
+   postIdWithPhotoId;
+   photosWithTags;
+   postInfo = loadInfo;
+   postPhotos = loadPhotos;
+}
+
+export const provider = new FlickrProvider();
+
+// export const provider: PostProvider = {
+//    photoBlog: loadPhotoBlog,
+//    exif: loadEXIF,
+//    postIdWithPhotoId,
+//    photosWithTags,
+//    postInfo: loadInfo,
+//    postPhotos: loadPhotos
+// };
